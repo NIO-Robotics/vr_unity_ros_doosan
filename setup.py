1c3006a8ed2d361ddb223fd4c02d572b930f3ff6
PFE25 <marc-arthur.georges.1@ens.etsmtl.ca>
2025-06-12 15:50:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ package_name = 'unity_ros_doosan'
 setup(
     name=package_name,
     version='0.0.0',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
