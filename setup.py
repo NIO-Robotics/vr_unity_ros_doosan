d9db88027af0f090d2e78d2fd8a6fab1d07d3f3c
PFE25 <marc-arthur.georges.1@ens.etsmtl.ca>
2025-06-12 14:03:30 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'unity_ros_doosan'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*launch.[pxy][yma]*')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='coro',
+    maintainer_email='guillaume.dupoiron@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
